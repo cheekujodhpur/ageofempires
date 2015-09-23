f51a701900f240c19b0e6929e38bcf229ab3d010
Kumar Ayush <cheekujodhpur@gmail.com>
2015-09-23 15:25:15 +0530

    initial variables and functions

---
+++ src/functions.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+import numpy as np
+import variables
+
+rhs_functions = []
+
+def f(X):
+    return 2
+
+rhs_functions.append(f)
+
+print rhs_functions
---
+++ src/variables.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+#What are our variables? Resources and population
+X_model = ['food','wood','population']
+
+#Needed to map variable names to indices
+def describe(X, variable_name, value):
+    X[X_model.index(variable_name)] = value
+
+
+#Total Number of variables, + 1 to account for time
+k = len(X_model) + 1
+
+#the ndarray to store initial conditions
+X = np.zeros(k)
+
+#initial time, does NOT assume to be 0
+t_1 = 0.0       #USERDEF
+
+#final time, anything greater than t_1
+t_2 = 10.0      #USERDEF
+
+#check
+if t_1>=t_2:
+    raise ValueError('Initial time cannot be greater than final time')
+
+#assign the initial time at the end of the array
+X[k-1] = t_1
+
+describe(X, 'food', 2)          #USERDEF
+describe(X, 'wood', 1)          #USERDEF
+describe(X, 'population', 10)   #USERDEF
+
+#flag OK
+print "Initial conditions defined..."
