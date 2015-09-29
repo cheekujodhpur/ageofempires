c407d17131f01e03834004f10ce69f936b725bcf
Kumar Ayush <cheekujodhpur@gmail.com>
2015-09-29 13:03:27 +0530

    added color to 3d and axis label

--- src/viz.py
+++ src/viz.py
@@ -37,6 +37,8 @@ import functions
 def phase_2D(results,variable_x,variable_y,variable_color=None,ax_xlim=None,ax_ylim=None):
     fig,ax = plt.subplots()
     jet = cm = plt.get_cmap('jet')
+    ax.set_xlabel(variable_x)
+    ax.set_ylabel(variable_y)
     
     if(variable_color):
         cNorm = colors.Normalize(vmin = variables.X_0[functions.map_vars(variable_color)][0], vmax = variables.X_0[functions.map_vars(variable_color)][1]) 
@@ -56,9 +58,18 @@ def phase_2D(results,variable_x,variable_y,variable_color=None,ax_xlim=None,ax_y
     plt.show()
 
 #function to plot three fields, for varying initial conditions
-def phase_3D(results,variable_x,variable_y,variable_z,ax_xlim=None,ax_ylim=None,ax_zlim=None):
+def phase_3D(results,variable_x,variable_y,variable_z,variable_color,ax_xlim=None,ax_ylim=None,ax_zlim=None):
     fig = plt.figure()
     ax = fig.gca(projection='3d')
+    jet = cm = plt.get_cmap('jet')
+    ax.set_xlabel(variable_x)
+    ax.set_ylabel(variable_y)
+    ax.set_zlabel(variable_z)
+    
+    if(variable_color):
+        cNorm = colors.Normalize(vmin = variables.X_0[functions.map_vars(variable_color)][0], vmax = variables.X_0[functions.map_vars(variable_color)][1]) 
+        scalarMap = cmx.ScalarMappable(norm=cNorm,cmap=jet)
+
 
     #define axis limits if specified
     if(ax_xlim):
@@ -69,7 +80,10 @@ def phase_3D(results,variable_x,variable_y,variable_z,ax_xlim=None,ax_ylim=None,
         ax.set_zlim(ax_zlim)
         
     for result in results:
-        ax.plot(result[:,functions.map_vars(variable_x)],result[:,functions.map_vars(variable_y)],zs=result[:,functions.map_vars(variable_z)])
+        if(variable_color):
+            ax.plot(result[:,functions.map_vars(variable_x)],result[:,functions.map_vars(variable_y)],zs=result[:,functions.map_vars(variable_z)],c=scalarMap.to_rgba(result[:,functions.map_vars(variable_color)][0]))
+        else:
+            ax.plot(result[:,functions.map_vars(variable_x)],result[:,functions.map_vars(variable_y)],zs=result[:,functions.map_vars(variable_z)],c='black')
     plt.show()
 
 #function to plot given field against time, for varying initial conditions
