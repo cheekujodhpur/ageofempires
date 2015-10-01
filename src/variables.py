24055547c5ea42860efa3104b8ce41a79d7f4a7b
Kumar Ayush <cheekujodhpur@gmail.com>
2015-10-01 19:53:50 +0530

    interesting phase plot. TODO: Area growth should be limited on surroundings rather than what people can sustain

--- src/variables.py
+++ src/variables.py
@@ -34,9 +34,9 @@ if t_0 >= t_1:
 #assign the initial time at the end of the array
 X_0[k-1] = [t_0,t_0+init_step]
 
-describe(X_0, 'population', [1,2])   #USERDEF
+describe(X_0, 'population', [0,11])   #USERDEF
 describe(X_0, 'food', [2,3])          #USERDEF
-describe(X_0, 'area', [1,2])          #USERDEF
+describe(X_0, 'area', [10,20])          #USERDEF
 
 #flag OK
 print "Initial conditions defined..."
